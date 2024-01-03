ca9aaa76f2b1d76924e4e43629d82582b5b4a5ae
deepakskepit <148887247+deepakskepit@users.noreply.github.com>
2024-01-03 21:11:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,10 @@ def run_selenium(logpath):
     with webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath)) as driver:
         url = "https://www.facebook.com/?sk=welcome"
         driver.get(url)
+        button = driver.find_element(By.CSS_SELECTOR, 'button[data-testid="royal_login_button"]')
+        name=button.text
         
-    return driver.page_source
+    return name
 
 
 if __name__ == "__main__":
