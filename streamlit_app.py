52bc1899af500c5ec961aeab67b485573fe97d6a
kavin-create <79439252+kavin-create@users.noreply.github.com>
2024-01-04 21:35:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def get_chromedriver_path():
 @st.cache_resource(show_spinner=False)
 def get_webdriver_options():
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
