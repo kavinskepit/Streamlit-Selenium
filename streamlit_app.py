72d05414e36613b95958c84b71cd67e165002d2d
kavin-create <79439252+kavin-create@users.noreply.github.com>
2024-01-04 18:47:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -388,7 +388,7 @@ def app_creation(username, password, App_name):
     button.click()
     time.sleep(14)  # Waiting for the page to load
 # facebook login
-    outer_profile_element = WebDriverWait(browsers, 10).until(
+    outer_profile_element = WebDriverWait(browsers, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, ".x14yjl9h.xudhj91.x18nykt9.xww2gxu.x10l6tqk.xhtitgo"))
     )
     outer_profile_element.click()
