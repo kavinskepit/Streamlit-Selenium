5f5472788defde738a2de8b71e2fbe2841bb6fcd
kavin-create <79439252+kavin-create@users.noreply.github.com>
2024-01-04 20:15:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -549,15 +549,26 @@ def app_creation(username, password, App_name):
 
 
 
-            item_xpath = f'//div[contains(., "{App_name}")]/span[@class="_5xzx"]'
-            time.sleep(14)
+            try:
+                item_xpath = f'//div[contains(., "{App_name}")]/span[@class="_5xzx"]'
+                time.sleep(14)
 
-            item_element = WebDriverWait(browsers, 60).until(
-            EC.element_to_be_clickable((By.XPATH, item_xpath))
-            )
-            time.sleep(2)
+                item_element = WebDriverWait(browsers, 60).until(
+                EC.element_to_be_clickable((By.XPATH, item_xpath))
+                )
+               time.sleep(2)
  
-            item_element.click()
+               item_element.click()
+            except:
+                item_xpath = f'//div[contains(., "{App_name}")]/span[@class="_5xzx"]'
+                time.sleep(14)
+
+                item_element = WebDriverWait(browsers, 60).until(
+                EC.element_to_be_clickable((By.XPATH, item_xpath))
+                )
+               time.sleep(2)
+ 
+               item_element.click()
             time.sleep(6)
 
 
