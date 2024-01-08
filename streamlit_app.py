853f926d875e1d51809e71971c13aafa23f625df
kavin-create <79439252+kavin-create@users.noreply.github.com>
2024-01-08 16:02:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,26 +110,26 @@ def content_generator(restuarant_name, location, nature_of_cuisine, occasion, of
 
 @st.cache_resource(show_spinner=False, experimental_allow_widgets=True)
 def image_generator(other_keywords):
-    max_wait_time=300
-    api_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IjMyYTZhMmFkZDhlMWIyODdjODI1NGM4MmU0OTVjM2UzIiwiY3JlYXRlZF9hdCI6IjIwMjQtMDEtMDRUMDY6MTg6NTQuNjMxODM4In0.LYY0PAaj4F0dj25V2elQaErz8u7pZJITnhL9qAc2lx8'  # Your API key here
+    #max_wait_time=300
+    #api_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IjMyYTZhMmFkZDhlMWIyODdjODI1NGM4MmU0OTVjM2UzIiwiY3JlYXRlZF9hdCI6IjIwMjQtMDEtMDRUMDY6MTg6NTQuNjMxODM4In0.LYY0PAaj4F0dj25V2elQaErz8u7pZJITnhL9qAc2lx8'  # Your API key here
     
-    monster_client = client(api_key)
-    model = 'sdxl-base'
-    input_data = {
-        'prompt': other_keywords,
-        'negprompt': 'unreal, fake, meme, joke, disfigured, poor quality, bad, ugly, text, letters, numbers, humans',
-        'samples': 2,
-        'steps': 50,
-        'aspect_ratio': 'square',
-        'guidance_scale': 7.5,
-        'seed': 2414,
-        }
-    result = monster_client.generate(model, input_data)
+    #monster_client = client(api_key)
+    #model = 'sdxl-base'
+    #input_data = {
+    #    'prompt': other_keywords,
+    #    'negprompt': 'unreal, fake, meme, joke, disfigured, poor quality, bad, ugly, text, letters, numbers, humans',
+    #    'samples': 2,
+    #    'steps': 50,
+    #    'aspect_ratio': 'square',
+    #    'guidance_scale': 7.5,
+    #    'seed': 2414,
+    #    }
+    #result = monster_client.generate(model, input_data)
     
     
 
-    image_urls = result['output']
-    #image_urls = ["https://www.simplilearn.com/ice9/free_resources_article_thumb/Coca_Cola_Marketing_Strategy_2022.jpg"]
+    #image_urls = result['output']
+    image_urls = ["https://www.simplilearn.com/ice9/free_resources_article_thumb/Coca_Cola_Marketing_Strategy_2022.jpg"]
     return image_urls
 
 
